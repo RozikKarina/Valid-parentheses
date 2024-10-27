dbd65fad4f474138e4c41ba12722d7c5c06fcf4c
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 03:09:36 +0200

    Добавьте файлы проекта.

---
+++ Valid-parentheses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0) return false;
+
+                char openBracket = stack.Pop();
+                if ((c == ')' && openBracket != '(') ||
+                    (c == '}' && openBracket != '{') ||
+                    (c == ']' && openBracket != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
